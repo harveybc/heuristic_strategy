99b93a589a7294d7916741cb60a14918c7f14ecf
Harvey Bastidas <harveybc@ingeni-us.com>
2025-02-08 04:09:58 -0500

    update

--- setup.py
+++ setup.py
@@ -1,33 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='predictor',
+    name='heuristic_strategy',
     version='0.1.0',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'predictor=app.main:main'
+            'heuristic_strategy=app.main:main'
         ],
-        'predictor.plugins': [
+        'heuristic_strategy.plugins': [
             'default=app.plugins.predictor_plugin_ann:Plugin',
             'ann=app.plugins.predictor_plugin_ann:Plugin',
             'cnn=app.plugins.predictor_plugin_cnn:Plugin',
             'lstm=app.plugins.predictor_plugin_lstm:Plugin',
             'transformer=app.plugins.predictor_plugin_transformer:Plugin',
-            'sarimax=app.plugins.predictor_plugin_sarimax:Plugin'
+            'sarimax=app.plugins.predictor_plugin_sarimax:Plugin',
+            'ls_pred_strategy=app.plugins.plugin_long_short_predictions:Plugin'
         ]
     },
     install_requires=[
-            'tensorflow-gpu',
-            'nvidia-cudnn-cu11',
-            'numpy',
-            'pandas',
-            'h5py',
-            'scipy',
-            'build',
-            'pytest',
-            'pdoc3',
-            'keras-multi-head'
+        
     ],
     author='Harvey Bastidas',
     author_email='your.email@example.com',
