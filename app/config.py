1995b97321e43c67246dec4a0c530170bfc3d368
Harvey Bastidas <harveybc@ingeni-us.com>
2025-02-10 04:42:26 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 # config.py
 
 DEFAULT_VALUES = {
-    #"hourly_predictions_file": "..\\predictor\\examples\\results\\phase_1\\phase_1_lstm_12600_1h_prediction.csv",
-    #"daily_predictions_file": "..\\predictor\\examples\\results\\phase_1_daily\\phase_1_cnn_6300_1d_prediction.csv",
+    "hourly_predictions_file": "..\\predictor\\examples\\results\\phase_1\\phase_1_lstm_12600_1h_prediction.csv",
+    "daily_predictions_file": "..\\predictor\\examples\\results\\phase_1_daily\\phase_1_cnn_6300_1d_prediction.csv",
     "hourly_predictions_file": None,
     "daily_predictions_file":  None,
     
