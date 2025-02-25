f53a5c2adc33758b31388b6f01e8e587778ef09a
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-02-24 22:30:23 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -14,7 +14,7 @@ DEFAULT_VALUES = {
     #"hourly_predictions_file": None,
     #"daily_predictions_file":  None,
     
-    "base_dataset_file": "tests\\data\\phase_1_base_d3.csv",
+    "base_dataset_file": "tests\\data\\phase_2_base_d3.csv",
     #"base_dataset_file": "tests\\data\\phase_1_base_d1.csv",
     "date_column": "DATE_TIME",
     "plugin": "default",
