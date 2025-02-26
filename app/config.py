6320a2ce2a87c0f6c79913b9a208f61746fa91af
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-02-26 02:01:17 -0500

    update

--- app/config.py
+++ app/config.py
@@ -1,11 +1,11 @@
 # config.py
 
 DEFAULT_VALUES = {
-    #"hourly_predictions_file": "tests\\data\\phase_2_3_cnn_1h_prediction_d3.csv",
-    #"daily_predictions_file": "tests\\data\\phase_2_3_cnn_1d_prediction_d3.csv",
+    "hourly_predictions_file": "tests\\data\\phase_2_3_cnn_1h_prediction_d3.csv",
+    "daily_predictions_file": "tests\\data\\phase_2_3_cnn_1d_prediction_d3.csv",
     #"daily_predictions_file": "tests\\data\\ann_predictions_daily_d3.csv",
-    "hourly_predictions_file": "tests\\data\\lstm_predictions_hourly_d1.csv",
-    "daily_predictions_file": "tests\\data\\cnn_predictions_daily_d1.csv",
+    #"hourly_predictions_file": "tests\\data\\lstm_predictions_hourly_d1.csv",
+    #"daily_predictions_file": "tests\\data\\cnn_predictions_daily_d1.csv",
     #"hourly_predictions_file": "tests\\data\\lstm_predictions_hourly_d3.csv",
     #"daily_predictions_file": "tests\\data\\cnn_predictions_daily_d3.csv",
     
@@ -14,7 +14,7 @@ DEFAULT_VALUES = {
     #"hourly_predictions_file": None,
     #"daily_predictions_file":  None,
     
-    #"base_dataset_file": "tests\\data\\phase_2_base_d3.csv",
+    "base_dataset_file": "tests\\data\\phase_2_3_base_d3.csv",
     #"base_dataset_file": "tests\\data\\phase_1_base_d1.csv",
     "date_column": "DATE_TIME",
     "plugin": "default",
